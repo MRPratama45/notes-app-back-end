e6e0853aba10531add505b6ec80ccf1ec0d93bd8
Muhammad Rizki Pratama <mrpratama45@gmail.com>
2022-12-22 16:27:04 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: "localhost",
+    /**
+     * host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+     * digunakan untuk proses production, karena agar dapat di akses secara public
+     * sehinggan nilai host akan bernilai sesuai dengan environmennya
+     * lalu tambahkan ini ("start-prod": "NODE_ENV=production node ./src/server.js",) ke package.json */
     routes: {
       cors: {
         origin: ["*"],
